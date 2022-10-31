12465914357d1d479cc25d5cf76d55e5ac9f2d09
maddenlockin <maddenlockin@gmail.com>
2022-10-31 11:03:18 -0700

    reset functionality added to event with transform on shells

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ const img3 = document.querySelector('#img-3');
 
 /* Events */
 button1.addEventListener('click', () => {
+    resetPearls();
     // generate a random location for the pearl (number between 1 and 3)
     const pearlLocation = Math.ceil(Math.random() * 3);
-    console.log('pearLocation', pearlLocation);
+    console.log('pearlLocation', pearlLocation);
     // add reveal class to img that lines up with random location (if..else)
     if (pearlLocation === 1) {
         img1.classList.add('reveal');
@@ -23,6 +24,11 @@ button1.addEventListener('click', () => {
     }
 });
 
+function resetPearls() {
+    img1.classList.remove('reveal');
+    img2.classList.remove('reveal');
+    img3.classList.remove('reveal');
+}
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
